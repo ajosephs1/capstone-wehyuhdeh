93b41f744afaba92ca7b78c5de45de05bea86341
ajosephs1 <77748724+ajosephs1@users.noreply.github.com>
2023-05-10 15:26:37 -0400

    Initial commit

---
+++ src/pages/RegisterPage.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import "./RegisterPage.scss";
+
+function RegisterForm() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // handle form submission here
+  };
+
+  return (
+    <form className="register-form" onSubmit={handleSubmit}>
+      <div className="register-form__field">
+        <label htmlFor="firstName">First Name:</label>
+        <input
+          type="text"
+          id="firstName"
+          value={firstName}
+          onChange={(event) => setFirstName(event.target.value)}
+        />
+      </div>
+      <div className="register-form__field">
+        <label htmlFor="lastName">Last Name:</label>
+        <input
+          type="text"
+          id="lastName"
+          value={lastName}
+          onChange={(event) => setLastName(event.target.value)}
+        />
+      </div>
+      <div className="register-form__field">
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        />
+      </div>
+      <div className="register-form__field">
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+      </div>
+      <div className="register-form__field">
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+      </div>
+      <div className="register-form__field">
+        <label htmlFor="confirmPassword">Confirm Password:</label>
+        <input
+          type="password"
+          id="confirmPassword"
+          value={confirmPassword}
+          onChange={(event) => setConfirmPassword(event.target.value)}
+        />
+      </div>
+      <button type="submit">Register</button>
+    </form>
+  );
+}
+
+export default RegisterForm;
