ded49dedc33849c180ad4387fd5e753079077f76
ajosephs1 <77748724+ajosephs1@users.noreply.github.com>
2023-05-16 23:51:59 -0400

    Final commit

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,15 +1,29 @@
-import React, { useState } from "react";
+import axios from 'axios';
+import { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import { BASE_URL } from "../../utils";
 import Logo from "../assets/icons/wehyuhdeh-logo-teal.svg"
 import "./LoginPage.scss";
 
 function LoginPage() {
-
   const [formInputs, setFormInputs] = useState("");
+  const { username, password } = formInputs
+  const navigate = useNavigate()
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(formInputs)
-  };
+    console.log(username, password)
+
+    axios.post(`${BASE_URL}/api/profile/login`, {
+      username: username,
+      password: password
+    })
+      .then((response) => {
+        sessionStorage.setItem("token", response.data.token);
+      })
+      .catch(error => console.log(error));
+      navigate('/');
+    };
 
   return (
     <div className="login">
@@ -20,28 +34,31 @@ function LoginPage() {
 
       <form className="login-form" onSubmit={handleSubmit}>
         <h1 className="login-form__header">Log In</h1>
-        
+
         <div className="login-form__field">
           <label htmlFor="username" className="login-form__label">Username</label>
           <input
             type="text"
             id="username"
             onChange={(event) => setFormInputs({ ...formInputs, username: event.target.value })}
-            className = "login-form__input"
+            className="login-form__input"
           />
         </div>
-        
+
         <div className="login-form__field">
           <label htmlFor="password" className="login-form__label">Password</label>
           <input
             type="password"
             id="password"
             onChange={(event) => setFormInputs({ ...formInputs, password: event.target.value })}
-            className = "login-form__input"
+            className="login-form__input"
           />
         </div>
-        
-        <button type="submit" className = "login-form__button">LOG IN</button>
+
+        <button type="submit" className="login-form__button">LOG IN</button>
+        <p>
+          Need an account? <Link to="/register">Sign up</Link>
+        </p>
       </form>
     </div>
   );
