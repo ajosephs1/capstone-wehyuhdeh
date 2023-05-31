40b01199af07d3f6952625e2412152d855b0599f
ajosephs1 <77748724+ajosephs1@users.noreply.github.com>
2023-05-31 14:06:04 -0400

    Add directions feature

--- src/components/PlaceCard/PlaceCard.js
+++ src/components/PlaceCard/PlaceCard.js
@@ -1,12 +1,29 @@
 import "./PlaceCard.scss"
 
-function PlaceCard({name, rating, address, photoRef }) {
+function PlaceCard({ name, rating, address, photoRef, url }) {
+
+    function urlEncode(addressString) {
+        const encodedChars = [];
+        for (let i = 0; i < addressString.length; i++) {
+          const char = addressString.charAt(i);
+          if (/[\w\-_.~]/.test(char)) {
+            encodedChars.push(char);
+          } else {
+            const charCode = char.charCodeAt(0);
+            encodedChars.push('%' + charCode.toString(16).toUpperCase());
+          }
+        }
+        return encodedChars.join('');
+      }
+      
     return (
         <div className="card">
             <h1 className="card__name">{name}</h1>
             <img src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=${photoRef}&key=AIzaSyDtF6_spNsgvJGhTviepX9IkbZRYECwl9o`} alt="place" className="card__photo" />
-            <p className="card__rating">{rating}</p>
-            <p className="card__location">{address}</p>
+            <p className="card__rating">rating: {rating}</p>
+            <a href={`https://www.google.com/maps/dir/?api=1&destination=${urlEncode(address)}&travelmode=driving`} target='#' className="card__directions">
+                <p className="card__location">{address}</p>
+            </a>
         </div>
     )
 }
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import Logo from "../assets/icons/wehyuhdeh-logo-home.svg"
+import Avatar from "../assets/icons/user.png"
 import PlacesList from "../../components/PlacesList/PlacesList";
 import "./HomePage.scss";
 
@@ -21,15 +22,20 @@ function HomePage() {
   function handleLocation(locationVal) {
     setLocation(coordinates[locationVal])
     console.log(location)
-    navigate("/location")
+    navigate(`/${locationVal}`)
   }
-
-
 
   return (
     <div className="homePage">
       <header className="homePage-header">
-        <img className="homePage-header__logo" src={Logo} alt="WehYuhDeh logo" />
+        <img className="homePage-header__logo" src={Logo} alt="WehYuhDeh logo" onClick={() => {
+          setLocation(null)
+          navigate('/')
+        }} />
+        <div className="homePage-header__user" onClick={()=> navigate('/profile/andrew')}>
+          <img src={Avatar} alt="avatar icon" className="homePage-header__avatar" />
+          <p className="homePage-header__username">Andrew</p>
+        </div>
       </header>
       <div className="button-container">
         <div className={`button-container__button ${location === "25.76228811240331,-80.20062164338918" ? "button-container__button--selected" : ""}`} onClick={() => { handleLocation('miami') }}><h2 className="button-container__location">MIAMI</h2></div>
@@ -39,18 +45,16 @@ function HomePage() {
         <div className={`button-container__button ${location === "51.50852523882869,-0.125829834899074" ? "button-container__button--selected" : ""}`} onClick={() => { handleLocation('london') }}><h2 className="button-container__location">LONDON</h2></div>
 
       </div>
-      <section className="card-container" id="card-container">
-        {/*
-         have state for each button click, button click triggers state change for content 
-         each click triggers an axios request is there a better way to save it in state 
-         save in state after initial axios request to not trigger multiple requests therefore if state is null make an axios request if not axios request 
-         if state is location render cards
-         cards map function  
-  */}
-        <Routes>
-          <Route path="/:location" element={<PlacesList location={location} />} />
-        </Routes>
-      </section>
+      {!location ? <section className="homePage__welcome">
+        <h2 className="homePage__welcome__heading">Welcome to WehYuhDeh</h2>
+        <p className="homePage__welcome__text">WehYuhDeh was created for the Jamaican diaspora to get a taste of home while they are abroad. This app is meant to create community for the diaspora or anyone wanting to get a taste of Jamaica's rich culture!</p>
+        <p className="homePage__welcome__text">Find Jamaican restaurants in a particular city by simply clicking on one of city toggles above. These cities are some of the most heavily populated metropolitan areas with a large Jamaican demographic.</p>
+      </section> :
+        <section className="card-container" id="card-container">
+          <Routes>
+            <Route path="/:location" element={<PlacesList location={location} />} />
+          </Routes>
+        </section>}
     </div>
   );
 }
